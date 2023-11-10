a0186b424e999b8daec82cd257a18f8d78bc69bf
EliasDevis <60856010+EliasDevis@users.noreply.github.com>
2023-11-10 17:16:21 +0100

    Added custom color picker

--- src/components/Palette/ColorCreate/ColorCreate.tsx
+++ src/components/Palette/ColorCreate/ColorCreate.tsx
@@ -1,24 +1,60 @@
-import { useContext } from "preact/hooks";
+import { useContext, useState } from "preact/hooks";
 import { PaletteContext } from "../../../managers/palette";
 import { AppColor } from "../../../types/AppColor";
 import styles from "./ColorCreate.module.css"
+import { HexAlphaColorPicker, HexColorInput, HexColorPicker } from "react-colorful";
+import "./ColorPicker.css"
+import { Button } from "../../Button/Button";
 
 export function ColorCreate() {
-    const palette = useContext(PaletteContext)
+    const palette = useContext(PaletteContext);
+    const [color, setColor] = useState("#aabbcc");
+    const [open, setOpen] = useState(false);
+
+    function onSelect() {
+        setOpen(!open)
+
+        palette.addAndSelect(new AppColor(color))
+    }
+
+    function toggle() {
+        setOpen(!open)
+    }
 
     return (
         <div className={styles.wrapper}>
-            <input
+            {open ?
+                (<div className="color-picker">
+                    <HexColorPicker color={color} onChange={setColor} />
+                    <HexColorInput color={color} onChange={setColor} prefixed/>
+                    <div className={styles.buttons}>
+                        <Button onClick={onSelect}>
+                            Готово
+                        </Button>
+                        <Button onClick={toggle} type="danger">
+                            Отмена
+                        </Button>
+                    </div>
+     
+                </div>)
+                : null}
+
+
+            <button onClick={toggle} className={styles.button}>
+                <img
+                    width={15}
+                    height={15}
+                    src="/images/icons/plus.svg"
+                    className={styles.icon} />
+            </button>
+
+            {/* <input
                 type="color"
                 name="create-color"
                 className={styles.input}
                 value={AppColor.getRandom().toHex()}
-                onChange={e => palette.addAndSelect(new AppColor(e.currentTarget.value))}/>
-            <img 
-                width={15}
-                height={15}
-                src="/images/icons/plus.svg" 
-                className={styles.icon} />
+                onChange={e => palette.addAndSelect(new AppColor(e.currentTarget.value))}/> */}
+
         </div>
     )
 }
