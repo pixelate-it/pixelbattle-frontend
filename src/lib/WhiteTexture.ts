c53405ce99bc2164e681477a419f52db2f516a48
mirdukkkkk <karmulav@gmail.com>
2024-02-21 20:18:34 +0700

    Update WhiteTexture.ts

--- src/lib/WhiteTexture.ts
+++ src/lib/WhiteTexture.ts
@@ -1,4 +1,4 @@
-import { Texture } from "@pixi/core";
+import { ALPHA_MODES, FORMATS, SCALE_MODES, Texture } from "@pixi/core";
 
 
-export const WHITE_TEXTURE = Texture.fromBuffer(new Uint8Array([255, 255, 255, 255]), 1, 1);
+export const WHITE_TEXTURE = Texture.fromBuffer(new Uint8Array([255, 255, 255]), 1, 1, { alphaMode: ALPHA_MODES.NO_PREMULTIPLIED_ALPHA, format: FORMATS.RGB, scaleMode: SCALE_MODES.NEAREST });
