8b9ad41b581dff82b80c406f666cfbb49958017f
mirdukkkkk <150621959+sberprime@users.noreply.github.com>
2024-02-28 04:26:22 +0700

    use terser

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
                 main: resolve(__dirname, "index.html"),
                 404: resolve(__dirname, "404.html"),
             }
-        }
+        },
+        minify: "terser"
     }
 });
