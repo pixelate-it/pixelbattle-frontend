dd7f352b473b2939616c888235f78e947b89bd6f
EtherCD <ethernetcoder@gmail.com>
2024-08-08 13:12:12 +0300

    Improve animations on the site. WindowBox rewritten to work with div, label

--- src/components/WindowBox/WindowBox.tsx
+++ src/components/WindowBox/WindowBox.tsx
@@ -1,20 +1,33 @@
 import { ComponentChildren } from "preact";
 import styles from "./WindowBox.module.css";
+import { useEffect, useRef, useState } from "preact/hooks";
 
 interface WindowProps {
-    children: ComponentChildren;
-    title: string;
-    showTip?: boolean
+	children: ComponentChildren;
+	title: string;
+	showTip?: boolean;
 }
 
 export function WindowBox({ children, title }: WindowProps) {
-    return (
-        <details className={styles.window}>
-            <summary className={styles.title}>{title}</summary>
+	const [opened, setOpened] = useState<boolean>(false);
+	const [show, setShow] = useState<boolean>(false);
 
-            <div className={styles.content}>
-                {children}
-            </div>
-        </details>
-    )
+	const click = () => {
+		setOpened(!opened);
+	};
+
+	useEffect(() => {
+		setTimeout(() => {
+			setShow(true);
+		}, 1000);
+	}, []);
+
+	return (
+		<div className={`${styles.window} ${opened ? styles.opened : styles.closed}`}>
+			<label htmlFor={styles.window} className={styles.title} onClick={click}>
+				{title}
+			</label>
+			<div className={styles.content}>{show && <div className={styles.container}>{children}</div>}</div>
+		</div>
+	);
 }
