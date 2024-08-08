ec856687cf6d8f7be108aa5c70bdd38e6d87e641
EtherCD <ethernetcoder@gmail.com>
2024-08-08 13:12:12 +0300

    Improve animations on the site. WindowBox rewritten to work with div, label

--- src/components/Bar/TitleBar/TitleBar.tsx
+++ src/components/Bar/TitleBar/TitleBar.tsx
@@ -8,50 +8,62 @@ import { Snowflake } from "../../Snow/Snowflake/Snowflake";
 import { Icon } from "../../General/Icon/Icon";
 
 export function TitleBar() {
-    const info = useContext(InfoContext)
-    const place = useContext(PlaceContext)
-    const [_infoIntervalId, setInfoIntervalId] = useState<NodeJS.Timeout>()
+	const info = useContext(InfoContext);
+	const place = useContext(PlaceContext);
+	const [_infoIntervalId, setInfoIntervalId] = useState<NodeJS.Timeout>();
+	const [opened, setOpened] = useState<boolean>(false);
+	const [show, setShow] = useState<boolean>(false);
 
-    useEffect(() => {
-        info.fetch();
+	useEffect(() => {
+		info.fetch();
 
-        setInfoIntervalId(setInterval(info.fetch, config.time.update.info))
-    }, [])
+		setInfoIntervalId(setInterval(info.fetch, config.time.update.info));
 
-    if (info.info.value === null || place.image.value === null) {
-        return null
-    }
+		setTimeout(() => {
+			setShow(true);
+		}, 1000);
+	}, []);
 
+	if (info.info.value === null || place.image.value === null) {
+		return null;
+	}
 
-    const isFinished = info.info.value.ended
+	const isFinished = info.info.value.ended;
 
-    const name = info.info.value.name === "season:blank" ? "Без названия" : info.info.value.name
-    const icon = isFinished ? "🏁" : "⚔️";
+	const name = info.info.value.name === "season:blank" ? "Без названия" : info.info.value.name;
+	const icon = isFinished ? "🏁" : "⚔️";
 
+	const click = () => {
+		setOpened(!opened);
+	};
 
-    return (
-        <details className={styles.wrapper}>
-            <summary className={styles.title}>{name} {icon}</summary>
-            <div className={styles.content}>
-                <div className={styles.params}>
-                    <Param label="Кулдаун" value={info.info.value.cooldown + "мс"} />
-                    <Param label="Размер" value={place.image.value.size.x + "x" + place.image.value.size.y} />
-                    <Param label="Онлайн" value={info.info.value.online.toString()} />
-                </div>
-                <div className={styles.icons}>
-                    <div className={styles.media}>
-                        {Object.entries(config.media).map(([name, url]) => (
-                            <a href={url[0]} target="_blank" rel="noopener noreferrer" key={name}>
-                                <Icon icon={name} alt={url[1]} size={35} viewBoxSize={256}/>
-                                {/* <img src={`/images/icons/${name}.svg`} alt={name} width={35} height={35}/> */}
-                            </a>
-                        ))}
-                    </div>
-                    <Snowflake />
-                </div>
-                
-
-            </div>
-        </details>
-    )
+	return (
+		<div className={`${styles.window} ${opened ? styles.opened : styles.closed}`} onClick={click}>
+			<label for={styles.window} className={styles.title}>
+				{name} {icon}
+			</label>
+			<div className={styles.content}>
+				{show && (
+					<div className={styles.container}>
+						<div className={styles.params}>
+							<Param label="Кулдаун" value={info.info.value.cooldown + "мс"} />
+							<Param label="Размер" value={place.image.value.size.x + "x" + place.image.value.size.y} />
+							<Param label="Онлайн" value={info.info.value.online.toString()} />
+						</div>
+						<div className={styles.icons}>
+							<div className={styles.media}>
+								{Object.entries(config.media).map(([name, url]) => (
+									<a href={url[0]} target="_blank" rel="noopener noreferrer" key={name}>
+										<Icon icon={name} alt={url[1]} size={35} viewBoxSize={256} />
+										{/* <img src={`/images/icons/${name}.svg`} alt={name} width={35} height={35}/> */}
+									</a>
+								))}
+							</div>
+							<Snowflake />
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
