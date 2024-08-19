41783c25bce578fab9fb4bf71c7c46d2b80e6375
EtherCD <ethernetcoder@gmail.com>
2024-08-19 23:55:42 +0900

    Fix routing... It's not over yet..

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { render } from 'preact';
-import { Switch, Route } from 'wouter-preact';
+import { render } from "preact";
+import { Switch, Route } from "wouter-preact";
 import { Suspense } from "preact/compat";
 
-import { Home } from './pages/Home';
-import { NotFound } from './pages/NotFound';
-import { Login } from './pages/Auth/Login';
-import { Logout } from './pages/Auth/Logout';
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { Login } from "./pages/Auth/Login";
+import { Logout } from "./pages/Auth/Logout";
 
 import "./styles/reset.css";
 import "./styles/font.css";
@@ -14,24 +14,22 @@ import styles from "./styles/index.module.css";
 
 export function App() {
 	return (
-        <Switch>
-            <Suspense fallback={
-                <div className={styles.wrapper}>
-                    <img
-                        className={styles.logo}
-                        src={'/images/meta/favicon.svg'}
-                        alt={'Логотип Pixel Battle'}
-                    />
-                    <p className={styles.message}>Loading</p>
-                </div>
-            }>
-                <Route path="/" component={Home} />
-            </Suspense>
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={Logout} />
-            <Route path={undefined} component={NotFound} />
-        </Switch>
+		<Suspense
+			fallback={
+				<div className={styles.wrapper}>
+					<img className={styles.logo} src={"/images/meta/favicon.svg"} alt={"Логотип Pixel Battle"} />
+					<p className={styles.message}>Загрузка</p>
+				</div>
+			}
+		>
+			<Switch>
+				<Route path="/" component={Home} />
+				<Route path="/login" component={Login} />
+				<Route path="/logout" component={Logout} />
+				<Route path={undefined} component={NotFound} />
+			</Switch>
+		</Suspense>
 	);
 }
 
-render(<App />, document.getElementById('app') as HTMLElement);
+render(<App />, document.getElementById("app") as HTMLElement);
